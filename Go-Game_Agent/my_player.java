e84213d6f5fa54fb2e4b4aaeffd24c48f76c97fb
Suyash Sardar <suyash.s.sardar@gmail.com>
2020-03-15 11:09:06 -0700

    March 15-1

--- Go-Game_Agent/my_player.java
+++ Go-Game_Agent/my_player.java
@@ -1,97 +1,333 @@
-import java.util.*;
-import java.io.*;
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileWriter;
+import java.io.IOException;
+import java.util.*;
 
-public class my_player {
-    Board previous_board;
-    Board current_board;
-    int player;
+class player {
+    int color; // 1 is black, 2 is white
+    int[][] prev;
+    int[][] curr;
 
-    class Position{
-        int x;
-        int y;
-        int liberty;
-        Position(int x, int y){
-            this.x = x;
-            this.y = y;
-            liberty=0;
-        }
-
-        @Override
-        public boolean equals(Object o) {
-            if (this == o) return true;
-            if (o == null || getClass() != o.getClass()) return false;
-            Position position = (Position) o;
-            return x == position.x &&
-                    y == position.y;
-        }
-
-        @Override
-        public int hashCode() {
-            return Objects.hash(x, y);
-        }
-    }
-    class Board{
-        int[][] board;
-
-        Board(int[][] board){
-            this.board = board;
-        }
+    player(int c, int[][] p, int[][] cur){
+        color = c;
+        prev = p;
+        curr = cur;
     }
 
-    my_player(){
-        previous_board = null;
-        current_board = null;
-        player = 0;
-    }
 
-    my_player(Board prev, Board curr, int player){
-        previous_board = prev;
-        current_board = curr;
-        this.player = player;
-    }
+    // >>>>>>>>>>>>>>>>>>>>>>>>>>>>>  Supporting functions  >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
-    public void readInput(my_player agent) throws IOException {
-        //int[][] prev = new int[][]{{0,0,0,0,0}, {0,0,0,0,0}, {0,0,0,0,0}, {0,0,0,0,0}, {0,0,0,0,0}};
-        //int[][] curr = new int[][]{{0,0,0,0,0}, {0,0,0,0,0}, {0,0,1,0,0}, {0,0,0,0,0}, {0,0,0,0,0}};
-        int[][] prev = new int[5][5];
-        int[][] curr = new int[5][5];
-        File intput_file = new File("input.txt");
-        Scanner sc = new Scanner(intput_file);
-        int player = Integer.parseInt(sc.nextLine());
-
-        prev = readArray(sc);
-        //sc.nextLine();
-        curr = readArray(sc);
-        /*print(prev);
-        System.out.println();
-        print(curr);
-        System.out.println();*/
-
-        agent = new my_player(new Board(prev), new Board(curr), player);
-
-        List<Position> ans = agent.validMoves(agent.current_board, player);
-
-        if(ans.size() != 0)
-            writeOutput(ans.get(0), true);
-        else
-            writeOutput(new Position(0, 0), false);
-
-        for(int i=0; i< ans.size(); i++){
-            System.out.println(ans.get(i).x + " " + ans.get(i).y + " " + ans.get(i).liberty);
-        }
-    }
-
-    public void print(int[][] arr){
-        for(int i= 0; i< 5; i++){
-            for(int j= 0; j< 5; j++){
-                System.out.print(arr[i][j]);
+    public boolean checkIfStart(){
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                if (curr[i][j] != 0) {
+                    return false;
+                }
             }
-            System.out.println();
+        }
+        return true;
+    }
+
+    public double partialArea(Board_State board, int player){
+        int count = 0;
+        for(int i =0; i<5;i++){
+            for(int j=0; j< 5; j++){
+                if(board.curr_board[i][j] == player){
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+
+    public List<Action> getAllPossiblePoints(Board_State board){
+        List<Action> ans = new ArrayList<>();
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                if(board.curr_board[i][j] == 0){
+                    ans.add(new Action(i, j));
+                }
+            }
+        }
+        return ans;
+    }
+
+    public List<Action> getPlayerCoins(Board_State board, int player){
+        List<Action> ans = new ArrayList<>();
+
+        for(int i =0; i<board.curr_board.length;i++){
+            for(int j=0; j< board.curr_board[0].length; j++){
+                if(board.curr_board[i][j] == player){
+                    ans.add(new Action(i, j));
+                }
+            }
+        }
+        return ans;
+    }
+
+    public List<Action> filterPoints(Board_State board, List<Action> allPoints, int player){
+        List<Action> ans = new ArrayList<>();
+
+        for(Action action: allPoints){
+
+            // Make the move
+            Board_State temp = new Board_State(board.prev_board, board.curr_board);
+            temp.curr_board[action.x][action.y] = player;
+
+            // Remove any opponent coins if required
+            removeOponentCoins(temp, 3-player);
+
+            // Check if it is a valid state
+            if(!zeroLiberty(temp, player) && !KOViolation(temp, player)){
+                // partialArea(temp, player);
+                ans.add(action);
+                calculateLiberty(temp, action, player);
+                action.liberty += utility(temp, player);
+            }
+            // Add to list if valid
+        }
+        Collections.sort(ans, (a, b)-> (int) (b.liberty - a.liberty));
+        return ans;
+    }
+
+    public boolean zeroLiberty(Board_State board, int player){
+        List<Action> coins = getPlayerCoins(board, player);
+
+        for(Action action: coins){
+            if(findLiberty(board, action, player)){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public boolean KOViolation(Board_State board, int player){
+        for(int i=0; i<5;i++){
+            for(int j=0;j<5;j++) {
+                if (board.curr_board[i][j] != board.prev_board[i][j]){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public void removeOponentCoins(Board_State temp, int player){
+        List<Action> allOponentCoins = getPlayerCoins(temp, player);
+
+        List<Action> ans = new ArrayList<>();
+
+        for(Action coin: allOponentCoins){
+            if(!findLiberty(temp, coin, player)){
+                ans.add(coin);
+            }
+        }
+        //remove zero liberty coins
+        for(Action coin: ans){
+            temp.curr_board[coin.x][coin.y] = 0;
         }
     }
 
-    public int[][] readArray(Scanner sc){
+    public boolean findLiberty(Board_State board, Action p,  int player){
+        int[][] temp = deepCopy(board.curr_board); //(n2)
+
+        List<Action> ally = allyDFS(board, player, p); //(n2)
+
+        for(Action coin: ally){
+            List<Action> neighbours = getNeighbours(coin);
+            for(Action neigh : neighbours){
+                if(temp[neigh.x][neigh.y] == 0){
+                    return true;
+                }
+
+            }
+        }
+        return false;
+    }
+
+    public List<Action> getNeighbours(Action p){
+        List<Action> neighbours = new ArrayList<>();
+        if(p.x>0) neighbours.add(new Action(p.x-1, p.y));
+        if(p.x<4) neighbours.add(new Action(p.x+1, p.y));
+        if(p.y>0) neighbours.add(new Action(p.x, p.y-1));
+        if(p.y<4) neighbours.add(new Action(p.x, p.y+1));
+        return neighbours;
+    }
+
+    public List<Action> getNeighboursAlly(Board_State board, Action p, int player){
+        List<Action> ans = new ArrayList<>();
+        List<Action> neighbours = getNeighbours(p);
+
+        for (Action neighbour : neighbours){
+            if(board.curr_board[neighbour.x][neighbour.y] == player){
+                ans.add(neighbour);
+            }
+        }
+        return ans;
+    }
+
+    public List<Action> allyDFS(Board_State board, int player, Action position){
+        Stack<Action> stack = new Stack<>();
+        List<Action> ally = new ArrayList<>();
+        stack.add(position);
+
+        while(!stack.isEmpty()){
+            Action p = stack.pop();
+            ally.add(p);
+            for(Action neighbour : getNeighboursAlly(board, p, player)){
+                if(!stack.contains(neighbour) && !ally.contains(neighbour)){
+                    stack.add(neighbour);
+                }
+            }
+        }
+        return ally;
+    }
+
+    public void setScores(List<Action> list, int player){
+        for(Action elem : list){
+            elem.score += elem.liberty;
+        }
+        Collections.sort(list, (a, b)-> (int) (b.score - a.score));
+    }
+
+    public void calculateLiberty(Board_State board, Action p, int player){
+        int liberty = 0;
+        int[][] temp = deepCopy(board.curr_board); //(n2)
+        temp[p.x][p.y] = player;
+        List<Action> ally = allyDFS(board, player, p); //(n2)
+
+        for(Action al: ally){
+            List<Action> neighbours = getNeighbours(al);
+            for(Action neigh : neighbours){
+                if(temp[neigh.x][neigh.y] == 0){
+                    temp[neigh.x][neigh.y] = -1;
+                    liberty+=1;
+                }
+
+            }
+        }
+        p.liberty = liberty;
+    }
+
+    // <<<<<<<<<<<<<<<<<<<<<<<<<<<<< Supporting functions <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+    //>>>>>>>>>>>>>>>>>>>>>>>>>>>> MinMax immediate supporting functions >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+    // Return most fruitful operations for a given board and a given player
+    public List<Action> goodActions(Board_State board, int player){
+        // All possible actions
+        List<Action> allPoints = getAllPossiblePoints(board);
+
+        // Filter - invalid moves
+        List<Action> filteredPoints = filterPoints(board, allPoints, player);
+
+        // Choose Top 5 moves
+        List<Action> ans = new ArrayList<>();
+
+        int branching_factor;
+
+        if(allPoints.size() >= 15){
+            branching_factor = 3;
+        }
+        else if (allPoints.size() > 7){
+            branching_factor = 10;
+        }
+        else{
+            branching_factor = allPoints.size();
+        }
+
+
+        for(int i=0; i<branching_factor && i < filteredPoints.size(); i++){
+            ans.add(filteredPoints.get(i));
+        }
+        return ans;
+    }
+
+    // Return the updated board given a prev board and action
+    public Board_State result(Board_State board, Action action, int player){
+        Board_State updated = new Board_State(board.prev_board, board.curr_board);
+        updated.prev_board = board.curr_board;
+        updated.curr_board[action.x][action.y] = player;
+        updated.level = board.level;
+        return updated;
+    }
+
+    public boolean is_terminal(Board_State board){
+        if(board.level == 3) return true;
+        return false;
+    }
+
+    public int utility(Board_State board, int player){
+        //Partial Area Score + Full Area Score
+        double count1, count2;
+        count1 = partialArea(board, player);
+        count2 = partialArea(board, 3-player);
+        if(player == 1){ // We are black
+            count2 += 2.5;
+            return (int)(count1 - count2);
+        }
+        else{
+            count1 += 2.5;
+            return (int)(count2 - count1);
+        }
+    }
+
+
+    // <<<<<<<<<<<<<<<<<<<<<<<<<<<<< MinMax immediate supporting functions <<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+    // >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>  MinMax  >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+    public Action minMaxDecision(Board_State board){
+        int max_score = Integer.MIN_VALUE;
+        int score = 0;
+        Action bestAction = new Action(-1, -1);
+
+        for(Action action: goodActions(board, color)){
+            score = minValue(result(board, action, color));
+            if(score > max_score){
+                max_score = score;
+                bestAction = action;
+            }
+        }
+        return bestAction;
+    }
+
+    public int maxValue(Board_State board){
+        if(is_terminal(board)) return utility(board, color);
+        board.level +=1;
+        int v = Integer.MIN_VALUE;
+        for(Action action : goodActions(board, color)){
+            v = Math.max(v, minValue(result(board, action, color)));
+        }
+        return v;
+    }
+
+    public int minValue(Board_State board){
+        if(is_terminal(board)) return utility(board, 3-color);
+        board.level +=1;
+        int v = Integer.MAX_VALUE;
+        for(Action action : goodActions(board, 3 - color)){
+            v = Math.min(v, maxValue(result(board, action, 3-color)));
+        }
+        return v;
+    }
+
+    // <<<<<<<<<<<<<<<<<<<<<<<<<<<<< MinMax <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+    public static int[][] deepCopy(int[][] a){
+        int[][] ans = new int[a.length][a[0].length];
+        for(int i=0; i< a.length; i++){
+            for(int j =0; j< a[0].length; j++){
+                ans[i][j] = a[i][j];
+            }
+        }
+        return ans;
+    }
+}
+
+public class my_player{
+    public static int[][] readArray(Scanner sc){
         int[][] ans = new int[5][5];
         for(int j= 0; j< 5; j++){
             String line = sc.nextLine();
@@ -104,142 +340,86 @@ public class my_player {
         return ans;
     }
 
-    public void writeOutput(Position p, boolean cont) throws IOException {
+    public static player readInput() throws FileNotFoundException {
+        File intput_file = new File("input.txt");
+        Scanner sc = new Scanner(intput_file);
+
+        int color = Integer.parseInt(sc.nextLine());
+        int[][] prev = readArray(sc);
+        int[][] curr = readArray(sc);
+
+        player agent = new player(color, prev, curr);
+        return agent;
+    }
+
+    public static void writeOutput(Action action) throws IOException {
         FileWriter myWriter = new FileWriter("output.txt");
-        if(cont == false){
+
+        if(action.x == -1 && action.y == -1){
             myWriter.write("PASS");
             myWriter.close();
             return;
         }
-        myWriter.write(p.x + "," + p.y);
+        myWriter.write(action.x + "," + action.y);
         myWriter.close();
     }
 
-    public List<Position> validMoves(Board board, int player){
-        if(checkIfStart()){
-            return new ArrayList(Arrays.asList(new Position(2, 2)));
-        }
-        List<Position> ans = new ArrayList<>();
-        List<Position> empty = getEmptyPlaces(board);
-        setLiberties(empty, player);
-        for(int i=0; i<5 && i<empty.size(); i++){
-            ans.add(empty.get(i));
-        }
-        return ans;
-    }
-
-    public boolean checkIfStart(){
-        for (int i = 0; i < 5; i++) {
-            for (int j = 0; j < 5; j++) {
-                if (current_board.board[i][j] != 0) {
-                    return false;
-                }
-            }
-        }
-        return true;
-    }
-
-    public void setLiberties(List<Position> list, int player){
-        for(Position elem : list){
-            elem.liberty = calculateLiberty(elem, player);
-        }
-        Collections.sort(list, (a, b)-> b.liberty - a.liberty);
-    }
-
-    public int countDead(Board prev, Board curr) {
-        int count = 0;
-        for (int i = 0; i < 5; i++) {
-            for (int j = 0; j < 5; j++) {
-                if (prev.board[i][j] != curr.board[i][j]) {
-                    count++;
-                }
-            }
-        }
-        return count;
-    }
-
-    public List<Position> getEmptyPlaces(Board curr){
-        List<Position> ans = new ArrayList<>();
-        for (int i = 0; i < 5; i++) {
-            for (int j = 0; j < 5; j++) {
-                if(curr.board[i][j] == 0){
-                    ans.add(new Position(i, j));
-                }
-            }
-        }
-        return ans;
-    }
-
-    public int[][] deepCopy(int[][] a){
-        int[][] ans = new int[a.length][a[0].length];
-        for(int i=0; i< a.length; i++){
-            for(int j =0; j< a[0].length; j++){
-                ans[i][j] = a[i][j];
-            }
-        }
-        return ans;
-    }
-
-    public int calculateLiberty(Position p, int player){
-        int liberty = 0;
-        int[][] temp = deepCopy(current_board.board); //(n2)
-        temp[p.x][p.y] = player;
-        List<Position> ally = allyDFS(player, p); //(n2)
-
-        for(Position al: ally){
-            List<Position> neighbours = getNeighbours(al);
-            for(Position neigh : neighbours){
-                if(temp[neigh.x][neigh.y] == 0){
-                    temp[neigh.x][neigh.y] = -1;
-                    liberty+=1;
-                }
-
-            }
-        }
-        return liberty;
-    }
-
-    public List<Position> allyDFS(int player, Position position){
-        Stack<Position> stack = new Stack<>();
-        List<Position> ally = new ArrayList<>();
-        stack.add(position);
-
-        while(!stack.isEmpty()){
-            Position p = stack.pop();
-            ally.add(p);
-            for(Position neighbour : getNeighboursAlly(p, player)){
-                if(!stack.contains(neighbour) && !ally.contains(neighbour)){
-                    stack.add(neighbour);
-                }
-            }
-        }
-        return ally;
-    }
-
-    public List<Position> getNeighbours(Position p){
-        List<Position> neighbours = new ArrayList<>();
-        if(p.x>0) neighbours.add(new Position(p.x-1, p.y));
-        if(p.x<4) neighbours.add(new Position(p.x+1, p.y));
-        if(p.y>0) neighbours.add(new Position(p.x, p.y-1));
-        if(p.y<4) neighbours.add(new Position(p.x, p.y+1));
-        return neighbours;
-    }
-
-    public List<Position> getNeighboursAlly(Position p, int player){
-        List<Position> ans = new ArrayList<>();
-        List<Position> neighbours = getNeighbours(p);
-        for (Position neighbour : neighbours){
-            if(current_board.board[neighbour.x][neighbour.y] == player){
-                ans.add(neighbour);
-            }
-        }
-        return ans;
-    }
-
     public static void main(String[] args) throws IOException {
-        my_player agent = new my_player();
 
-        agent.readInput(agent);
+        player agent = readInput();
 
+        Action action;
+
+        if(agent.checkIfStart()){
+            action = new Action(2, 2);
+        }
+        else {
+            action = agent.minMaxDecision(new Board_State(agent.prev, agent.curr));
+        }
+
+        writeOutput(action);
+
+    }
+}
+
+
+class Board_State{
+    public int[][] prev_board;
+    public int[][] curr_board;
+    public int utility;
+    public int level;
+
+    Board_State(int[][] prev_board, int[][] curr_board){
+        this.prev_board = player.deepCopy(prev_board);
+        this.curr_board = player.deepCopy(curr_board);
+        this.utility = 1;
+    }
+}
+
+class Action{
+    int x;
+    int y;
+    int liberty;
+    int score;
+
+    Action(int x, int y){
+        this.x = x;
+        this.y = y;
+        liberty=0;
+        score=0;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Action position = (Action) o;
+        return x == position.x &&
+                y == position.y;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(x, y);
     }
 }
